8e83a07fb858a89436513704db4e93d8f09aa80a
Seu Nome <henriqsantos629@gmail.com>
2024-11-19 20:12:28 -0300

    created new files

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,19 +2,13 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "../pages/LoginScreen/LoginScreen";
 import HomeScreen from "../pages/HomeScreen";
 
-
-
 export  type RootStackParamList = {
     LoginScreen: undefined;
     HomeScreen: undefined;
     
 }
 
-
 const stack = createNativeStackNavigator<RootStackParamList>();
-
-
-
 
 export function Routes() {
     return (
